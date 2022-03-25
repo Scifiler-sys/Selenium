f596065cfeef044536a1d282ff3af71b3983b846
Scifiler-sys <pagdilaostephen@gmail.com>
2022-03-25 17:43:52 -0400

    Added startup

--- AutoStandup/Program.cs
+++ AutoStandup/Program.cs
@@ -1,10 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Diagnostics;
 
-// //Opens shortcut
-// ProcessStartInfo info = new ProcessStartInfo("Bot - Chrome.lnk");
-// info.UseShellExecute = true;
-// Process.Start(info);
+//Opens shortcut
+ProcessStartInfo info = new ProcessStartInfo("Bot - Chrome.lnk");
+info.UseShellExecute = true;
+Process.Start(info);
+
+Console.WriteLine("Starting the bot...");
+Thread.Sleep(1000);
 
 Driver chromeBrowser = new Driver();
 
