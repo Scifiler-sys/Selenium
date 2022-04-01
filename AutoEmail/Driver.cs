6741f5f046a2954aae562a05e9d97a6d4cb151e8
Scifiler-sys <pagdilaostephen@gmail.com>
2022-04-01 17:30:57 -0400

    Added description

--- AutoEmail/Driver.cs
+++ AutoEmail/Driver.cs
@@ -44,7 +44,8 @@ public class Driver
         //Switching to the iframe since Saleforce uses it
         _driver.SwitchTo().Frame(iframe);
 
-        string reportXPath = @"//*[@id='report-00O0P0000045vl3UAA']/div/div[1]/div[2]/div/div/div/div/div/div/div[1]/div/div/div[1]/div/div/div/div[4]/div/div/div/div/div/div/div[2]/table/tbody/tr";
+        //                  //*[@id="report-00O0P0000045vl3UAA"]/div/div[1]/div[2]/div/div/div/div/div/div/div[1]/div/div/div[1]/div/div/div/div[4]/div/div/div/div/div/div/div[2]/table/tbody
+        string reportXPath = @"//*[@id='report-00O0P0000045vl3UAA']/div/div[1]/div[2]/div/div/div/div/div/div/div[1]/div/div/div[1]/div/div/div/div[4]/div/div/div/div/div/div/div[1]/table/tbody/tr";
         var tableRows = _driver.FindElements(By.XPath(reportXPath));
 
         while (tableRows.Count < 5)
--- AutoStandup/readme.md
+++ AutoStandup/readme.md
@@ -1,5 +1,25 @@
 # Project Details
 * Using Selenium instead to automate standup and use pre-existing user credentials in Google Chrome
 
+# Project Setup
+1. Add appsettings.json and add this scaffold here
+```json
+{
+    "DateStartedBatch" : "DATE HERE",
+    "AssociateNumber": 0,
+    "SaleForceMatch": false, 
+    "SaleForceReasons": [
+        "NAME - REASON",
+        "NAME - REASON"
+    ],
+    "Warning": 0,
+    "GeneralNote" : "GENERAL DESC.",
+    "Initiatives": [
+        "INITIATIVES 1",
+        "INITIATIVES 2"
+    ]
+}
+```
+
 # Future Implementation
 * Go to caliber to dynamically change QC overall feedback for that week
